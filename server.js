fb67cf8b3da8dfc1b9a088c5113cf051771a2398
areebaaamir444-create <areebaaamir444@gmail.com>
2026-01-07 01:12:37 +0500

    some changes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 const PORT = 5000;
 
-// ✅ MUST BE BEFORE ROUTES
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
